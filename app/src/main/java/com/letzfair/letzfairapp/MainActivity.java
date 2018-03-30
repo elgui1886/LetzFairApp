09af8681ab3edc728a128e3a4ade3d65c41f327c
Elia Guidi <eguidi@terranovasoftware.eu>
2018-03-30 17:55:50 +0200

    Progetto App Letz Fair

---
+++ app/src/main/java/com/letzfair/letzfairapp/MainActivity.java
@@ -0,0 +1,25 @@
+package com.letzfair.letzfairapp;
+
+import android.content.Intent;
+import android.graphics.Paint;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button btn = findViewById(R.id.button2);
+        btn.setPaintFlags(btn.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
+    }
+
+    public void SkipLoginClick(View view)
+    {
+        Intent intent = new Intent(this, LetzFair_Home.class);
+        startActivity(intent);
+    }
+}
