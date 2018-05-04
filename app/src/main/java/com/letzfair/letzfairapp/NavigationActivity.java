94d74192d24d5916d2659d62466cff9984005706
Elia Guidi <eguidi@terranovasoftware.eu>
2018-05-04 17:56:05 +0200

    Prima versione compilante - da rivedere layout

--- app/src/main/java/com/letzfair/letzfairapp/NavigationActivity.java
+++ app/src/main/java/com/letzfair/letzfairapp/NavigationActivity.java
@@ -83,10 +83,6 @@ public class NavigationActivity extends AppCompatActivity
 
         } else if (id == R.id.nav_manage) {
 
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
